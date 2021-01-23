754a10f0a4dbb7969f2395fafd44bb91a08b686e
lzc <775269512@qq.com>
2021-01-23 17:25:07 +0800

    lc35

---
+++ src/array/lc35.java
@@ -0,0 +1,12 @@
+package array;
+
+public class lc35 {
+	public int searchInsert(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] >= target) {
+				return i;
+			}
+		}
+		return nums.length;
+	}
+}
