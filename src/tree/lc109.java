2f8a91d0c230b6bf66fa6222cc21929634020604
lzc <775269512@qq.com>
2021-01-27 23:38:51 +0800

    lc110

--- src/tree/lc109.java
+++ src/tree/lc109.java
@@ -1,7 +1,28 @@
 package tree;
 
+import util.ListNode;
+import util.TreeNode;
+
 public class lc109 {
 	public TreeNode sortedListToBST(ListNode head) {
-
+		if (head == null)
+			return null;
+		else if (head.next == null)
+			return new TreeNode(head.val);
+		ListNode pre = head;
+		ListNode p = pre.next;
+		ListNode q = p.next;
+		// 找到链表的中点p
+		while (q != null && q.next != null) {
+			pre = pre.next;
+			p = pre.next;
+			q = q.next.next;
+		}
+		// 将中点左边的链表分开
+		pre.next = null;
+		TreeNode root = new TreeNode(p.val);
+		root.left = sortedListToBST(head);
+		root.right = sortedListToBST(p.next);
+		return root;
 	}
 }
